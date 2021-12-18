a122ec05802893d3431dcdf1994be673cebb4758
Peter Störmer <fun@tempest-rock-studios.com>
2021-12-18 23:54:28 +0100

    Cool version of RGB lighting

--- pst-01/src/rgb-light.h
+++ pst-01/src/rgb-light.h
@@ -11,21 +11,19 @@
 
 extern void keyboard_post_init_user_rgb_light(void);  
 
+// The only mode we have in order to save memory space:
 #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 
-// The number of LEDs in total:
+// The total number of LEDs in both keyboards:
 #undef RGBLED_NUM
 #define RGBLED_NUM 20
 
-// define RGBLIGHT_ANIMATIONS
-// define RGBLIGHT_HUE_STEP  8
-// define RGBLIGHT_SAT_STEP  8
-// define RGBLIGHT_VAL_STEP  8
-// define RGBLIGHT_LIMIT_VAL 150
+// Define the order of the LEDs such that they - synchronously on both keyboards - go in a nice circle:
+#define RGBLIGHT_LED_MAP { 0, 1, 2, 9, 8, 7, 4, 3, 5, 6, 0, 1, 2, 9, 8, 7, 4, 3, 5, 6 }
 
 // Synchronize split keyboards
 #define RGBLIGHT_SPLIT
 
-#endif
+#endif /* RGBLIGHT_ENABLE */
 
 #endif /* RGB_LIGHT_H */
